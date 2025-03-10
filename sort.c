6adfd3d2239c724cb485a0862c49c19a80f7a1ba
nikitulka0120 <sternsisnikita@gmail.com>
2025-03-10 19:29:51 +0700

    added sort.c

---
+++ sort.c
@@ -0,0 +1,28 @@
+int bubbleSort(int n, int A[])
+{
+    // printf("Контрольная сумма до сортировки:%d\n",CheckSum(n, A));
+    // printf("Число серий до сортировки:%d\n",RunNumber(n, A));
+    int C = 0;
+    int M = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            C++;
+            if (A[j] < A[j - 1])
+            {
+                M++;
+                int temp = A[j];
+                M++;
+                A[j] = A[j-1];
+                M++;
+                A[j-1] = temp;
+            }
+        }
+    }
+    // printf("Контрольная сумма после сортировки:%d\n",CheckSum(n, A));
+    // printf("Число серий после сортировки:%d\n",RunNumber(n, A));
+    int trud = M + C;
+    return trud;
+
+}
